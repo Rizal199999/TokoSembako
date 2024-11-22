d9f0a5acecac2f0e33ea2f5c9e7e80b82f7387cc
M Wahyudi HD <muh.wahyudi282828@gmail.com>
2024-11-22 22:11:38 +0700

    test:  test CRUD

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -7,8 +7,8 @@ use CodeIgniter\Router\RouteCollection;
  */
 $routes->get('/', 'Home::index');
 $routes->get('/base', 'Home::test');
-$routes->get('products/add', 'ProductsController::add');
-$routes->post('products/save', 'ProductsController::save');
+$routes->get('/products/add', 'ProductsController::add');
+$routes->post('/products/save', 'ProductsController::save');
 $routes->get('/products/search_query', 'ProductsController::search_ajax');
 $routes->get('/products', 'ProductsController::index');
 $routes->post('/products/update/(:num)', 'ProductsController::update/$1'); // Rute untuk update produk
